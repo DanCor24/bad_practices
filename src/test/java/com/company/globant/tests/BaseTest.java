03f4c83d4998016536cb5cd5e059949a780005bd
DanRCM <rcortezmanzano24@gmail.com>
2024-02-13 19:40:49 -0500

    POM corrections

--- src/test/java/com/company/globant/tests/BaseTest.java
+++ src/test/java/com/company/globant/tests/BaseTest.java
@@ -2,18 +2,10 @@ package com.company.globant.tests;
 
 import com.company.globant.pages.CartPage;
 import com.company.globant.pages.HomePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-
-import java.time.Duration;
 
 public class BaseTest {
     public HomePage homePage;
